0ec5099de35929d7426f79f607a9156714889c53
Emil Martinov <martinov.emil@gmail.com>
2019-03-20 17:12:18 +0100

    Menu is worked on and added separate css for menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,44 +1,78 @@
 import React from 'react';
+import './menu.css';
 // import { Link } from 'gatsby';
 
 const menuItems = [ 'Home', 'Rooms', 'Photos', 'About' ];
 
+// class MenuItems extends React.Component
 const MenuItems = () => {
+	// const MenuToggle = document.querySelector('.menu-toggle');
+	// const menuSection = document.querySelector('.menu-section');
+	// const MenuSection = document.createElement('div');
+	// const MenuSection = () => {
+	// 	return <div />;
+	// };
+	const MenuSection = React.createElement('div', null);
+	// const MenuToggle = document.createElement('div');
+	// const MenuToggle = () => {
+	// 	return <div />;
+	// };
+	const MenuToggle = React.createElement('div', null);
+	// const NavUl = document.createElement('ul');
+	// const NavUl = () => {
+	// 	return <ul />;
+	// };
+	// const NavUl = document.querySelector('nav').querySelector('ul');
+	const NavUl = React.createElement('ul', null);
+	console.log(MenuSection, MenuToggle, NavUl);
+	function toggleOnMenu(event) {
+		MenuToggle.classList.toggle('on');
+		MenuSection.classList.toggle('on');
+		navHide(event);
+		// event.stopPropagation();
+		// console.log(this);
+	}
+
+	function navHide(event) {
+		NavUl.classList.toggle('hidden');
+		console.log(event.target);
+		event.preventDefault();
+		// event.stopPropagation();
+	}
+
 	return (
-		<div
-			style={{
-				width: '40px',
-				height: '30px',
-				position: 'absolute',
-				top: '10px',
-				right: '25px',
-				cursor: 'pointer'
-			}}
-		>
-			<div style={{}}>
-				<div style={{}} />
-				<div style={{}} />
-				<div style={{}} />
-			</div>
+		// menu-section
+		<MenuSection onClick={toggleOnMenu} className="menu-section" style={{}}>
+			{/* menu-toggle */}
+			<MenuToggle className="menu-toggle" style={{}}>
+				{/* one */}
+				<div className="one" style={{}} />
+				{/* two */}
+				<div className="two" style={{}} />
+				{/* three */}
+				<div className="three" style={{}} />
+			</MenuToggle>
 			<nav>
-				<ul
+				<NavUl
 					role="navigation"
 					className="hidden"
 					style={{
-						display: 'none'
+						// display: 'none'
 					}}
 				>
 					{menuItems.map((menuItem) => {
 						return (
-							<li>
+							<li key={menuItem}>
 								<a href="/">{menuItem}</a>
 							</li>
 						);
 					})}
-				</ul>
+				</NavUl>
 			</nav>
-		</div>
+		</MenuSection>
 	);
 };
+
+// const MenuItems = () => {};
 
 export default MenuItems;
